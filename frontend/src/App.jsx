b2bd64a47b6fdb95e0f229abeb8cb8d6769d7ee5
ArthurH04 <92767264+ArthurH04@users.noreply.github.com>
2026-01-10 01:11:45 -0300

    fix: package.json vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import PawnExplorer from "./components/PawnExplorer"
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import PawnsPage from "./pages/PawnsList/PawnsPage";
+import Navbar from "./components/Navbar";
+import PawnPage from "./pages/PawnPage/PawnPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <PawnExplorer/>
+    <div>
+      
+      <Navbar/>
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/pawn/:id" element={<PawnPage/>} />
+        <Route path="/" element={<PawnsPage/>} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
